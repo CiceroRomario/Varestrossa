1c99ca273ee31aed08d39d739cb2f44e351e057f
Kaiowa <Kaiowa@Kaiowa>
2017-07-04 17:01:02 -0300

    Merge origin/master
    Conflicts:
    	index.php

---
+++ model/db_data.php
@@ -0,0 +1,8 @@
+<?php
+
+
+	//Variáveis de de configuração do banco de dados.
+	$GLOBALS['db_host'] = "localhost";
+	$GLOBALS['db_user'] = "root";
+	$GLOBALS['db_password'] = "0412";
+	$GLOBALS['db_name'] = "varestrossa";
